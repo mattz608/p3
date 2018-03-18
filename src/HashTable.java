84ba161c9adcef541f28098a490c325afae5d57c
Matt Zimmers <mzimmers@wisc.edu>
2018-03-18 18:39:21 -0500

    Init

---
+++ src/HashTable.java
@@ -0,0 +1,40 @@
+
+public class HashTable<K, V> implements HashTableADT<K, V> {
+    /* Instance variables and constructors
+     */
+
+    @Override
+    public V put(K key, V value) {
+        //TODO: Implement put method - using efficient algorithm
+        return null;
+    }
+
+    @Override
+    public void clear() {
+       //TODO: Implement this method
+    }
+
+    @Override
+    public V get(K key) {
+        //TODO: Implement the get method
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //TODO: Implement the method
+        return true;
+    }
+
+    @Override
+    public V remove(K key) {
+       //TODO: Implement the remove method
+        return null;
+    }
+
+    @Override
+    public int size() {
+        //TODO: Implement this method
+        return 0;
+    }
+}
